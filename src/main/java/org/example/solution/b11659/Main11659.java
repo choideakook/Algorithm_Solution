c664f4e62302ed03aaff99a5c8fd6f067defe01f
choideakook <shdrnrhd113@naver.com>
2023-06-13 10:59:44 +0900

    b11659: 스스로 리팩토링

--- src/main/java/org/example/solution/b11659/Main11659.java
+++ src/main/java/org/example/solution/b11659/Main11659.java
@@ -10,38 +10,23 @@ public class Main11659 {
         int N = sc.nextInt();
         int M = sc.nextInt();
 
-        int[] sum = new int[N];
-        sum[0] = sc.nextInt();
+        int[] sum = new int[N + 1];
+        sum[0] = 0;
 
-        for (int i = 1; i < N; i++)
+        for (int i = 1; i <= N; i++)
             sum[i] = sum[i - 1] + sc.nextInt();
 
         for (int i = 0; i < M; i++) {
             int fir = sc.nextInt();
             int last = sc.nextInt();
-            int answer = 0;
 
-            try {
-                answer = sum[last - 1] - sum[fir - 2];
-            } catch (Exception e) {
-                answer = sum[last - 1];
-            }
-            System.out.println(answer);
+            System.out.println(sum[last] - sum[fir - 1]);
         }
 
     }
 
     public int[] solution(int N, int M, int[] num, int[][] terms) {
         int[] answer = new int[M];
-        int[] sum = new int[N];
-        sum[0] = num[0];
-
-        for (int i = 1; i < N; i++)
-            sum[i] = sum[i - 1] + num[i];
-
-        for (int i = 0; i < M; i++)
-            answer[i] = sum[terms[i][1] - 1] - sum[terms[i][0] - 2];
-
 
         return answer;
     }
