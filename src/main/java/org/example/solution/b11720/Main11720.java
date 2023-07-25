6424a0883b33427b64bf0743eb6e0f488ca86e1b
choideakook <shdrnrhd113@naver.com>
2023-07-25 09:02:23 +0900

    b11720 : 해결

---
+++ src/main/java/org/example/solution/b11720/Main11720.java
@@ -0,0 +1,19 @@
+package org.example.solution.b11720;
+
+import java.util.*;
+
+public class Main11720 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int length = sc.nextInt();
+        char[] chars = sc.next().toCharArray();
+        long sum = 0;
+
+        for(char cha : chars)
+            sum += cha - '0';
+
+        System.out.println(sum);
+        sc.close();
+    }
+}
---
+++ src/test/java/org/example/solution/b11720/Main11720Test.java
@@ -0,0 +1,7 @@
+package org.example.solution.b11720;
+
+
+class Main11720Test {
+
+    Main11720 solution = new Main11720();
+}
