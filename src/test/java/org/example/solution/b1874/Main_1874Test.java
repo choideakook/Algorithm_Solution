9470687f1755ed9785c8508c90f25f2ea9d0efc5
choideakook <shdrnrhd113@naver.com>
2023-07-27 09:59:06 +0900

    b1874: 문제 해결

---
+++ src/test/java/org/example/solution/b1874/Main_1874Test.java
@@ -0,0 +1,16 @@
+package org.example.solution.b1874;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Stack;
+
+class Main_1874Test {
+
+
+    @Test
+    void name() {
+        Stack st = new Stack();
+        st.peek();
+    }
+}
