7ab1a76087736febe02611d153e5f274e946b6f0
choideakook <shdrnrhd113@naver.com>
2023-07-31 20:46:39 +0900

    b2042: 런타임 에러 나는데 어디가 문제인지 모르겠음

--- src/main/java/org/example/solution/baekjoon/b2042/Main_2042_1.java
+++ src/main/java/org/example/solution/baekjoon/b2042/Main_2042_1.java
@@ -8,62 +8,61 @@ package org.example.solution.baekjoon.b2042;
  * 구간 합 횟수 1 <= K <= 10,000
  * a = 1 일 때 b 번째를 c 로 변경
  * a = 2 일 때 b ~ c 까지 합을 구함
+ *
+ * 스케너로 수정
  */
 
 import java.util.*;
-import java.io.*;
 
-public class Main_2042 {
+public class Main_2042_1 {
 
-    static long[] tree;
-    static long result;
+    static int[] tree;
+    static int result;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int K = sc.nextInt();
 
         int square = 1;
         while (square < N) square *= 2;
 
-        tree = new long[square * 2];
+        tree = new int[square * 2];
         for (int i = square; i < square + N; i++)
-            tree[i] = Long.parseLong(br.readLine());
+            tree[i] = sc.nextInt();
 
         for (int i = tree.length - 1; i > 1; i--)
             tree[i/2] += tree[i];
 
         for (int i = 0; i < M + K; i++){
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int start = Integer.parseInt(st.nextToken()) + square - 1;
-            int c = Integer.parseInt(st.nextToken());
+            int a = sc.nextInt();
+            int start = sc.nextInt() + square - 1;
+            int c = sc.nextInt();
 
             if (a == 1) update(start, tree[start] - c);
 
             else {
-                sum(start, c + square - 1);
+                cal(start, c + square - 1);
                 System.out.println(result);
                 result = 0;
             }
         }
     }
 
-    private static void update(int index, long num){
+    private static void update(int index, int num){
         tree[index] -= num;
         if (index > 1) update(index / 2, num);
     }
 
-    private static void sum(int start, int end){
+    private static void cal(int start, int end){
         if(start != end) {
             if (start % 2 == 1) result += tree[start];
             if (end % 2 == 0) result += tree[end];
         }
 
-        if (start < end) sum((start + 1) / 2, (end - 1) / 2);
+        if (start < end) cal((start + 1) / 2, (end - 1) / 2);
 
         else if (start == end) result += tree[start];
     }
