4bf39a2ee20e4d18600637d00dfae9ba29b9ca37
choideakook <shdrnrhd113@naver.com>
2023-08-08 10:35:11 +0900

    b11725: BFS - 부모를 기록하는 int[] 로 시간 초과 문제 해결

--- src/main/java/org/example/solution/baekjoon/dfs_bfs/b11725/Main_11725.java
+++ src/main/java/org/example/solution/baekjoon/dfs_bfs/b11725/Main_11725.java
@@ -3,7 +3,7 @@ package org.example.solution.baekjoon.dfs_bfs.b11725;
 /**
  * 트리의 부모 찾기
  * 시간 제한 1초
- *
+ * <p>
  * N (2 <= N <= 100,000)
  */
 
@@ -13,13 +13,17 @@ import java.util.*;
 public class Main_11725 {
     static List<Integer>[] tree;
     static boolean[] visited;
+    static int[] parents;
     static int N;
-    public static void main(String[] args) throws IOException{
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
 
+        visited = new boolean[N + 1];
         tree = new ArrayList[N + 1];
+        parents = new int[N + 1];
 
         for (int i = 1; i < N + 1; i++)
             tree[i] = new ArrayList<>();
@@ -33,26 +37,24 @@ public class Main_11725 {
             tree[B].add(A);
         }
 
-        for(int i = 2; i < N + 1; i++) {
-            visited = new boolean[N + 1];
-            BFS(1, i);
-        }
+        BFS(1);
+
+        for (int i = 2; i < N + 1; i++)
+            System.out.println(parents[i]);
     }
-    static void BFS(int root, int target){
+
+    static void BFS(int root) {
         Queue<Integer> que = new LinkedList<>();
         visited[root] = que.offer(root);
 
         while (!que.isEmpty()) {
             int node = que.poll();
 
-            for(int i : tree[node])
-                if (i == target){
-                    System.out.println(node);
-                    return;
-                }
-
-                else if (!visited[i])
+            for (int i : tree[node])
+                if (!visited[i]){
                     visited[i] = que.offer(i);
+                    parents[i] = node;
+                }
         }
     }
 }
