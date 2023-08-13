1a7123d5e77a70704ff875cd3bbc67340bc4c72c
choideakook <shdrnrhd113@naver.com>
2023-08-13 19:47:46 +0900

    b2839: 문제 해결

--- src/main/java/org/example/solution/baekjoon/greedy/b2839/Main_2839.java
+++ src/main/java/org/example/solution/baekjoon/greedy/b2839/Main_2839.java
@@ -13,16 +13,17 @@ import java.util.*;
 public class Main_2839 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] potion = {5, 3};
         int N = sc.nextInt();
-        int result = 0;
+        int result = -1;
 
-        for(int i : potion){
-            result += N / i;
-            N = N % i;
+        for (int i = 0; i < 5; i++) {
+            int rest = N -(i * 3);
+
+            if (rest % 5 == 0){
+                result = i + (rest / 5);
+                break;
+            } else if (i * 3 > N) break;
         }
-
-        if (N != 0) result = -1;
         System.out.println(result);
     }
 }
