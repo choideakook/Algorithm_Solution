6409e5307bf97e3eb1e2e40576080b17c8672db3
choideakook <shdrnrhd113@naver.com>
2023-08-20 17:14:58 +0900

    b13913: 객체 방식에서 배열 방식으로 변경 - 문제 해결

--- src/main/java/org/example/solution/baekjoon/dfs_bfs/b13913/Main_13913.java
+++ src/main/java/org/example/solution/baekjoon/dfs_bfs/b13913/Main_13913.java
@@ -11,73 +11,57 @@ package org.example.solution.baekjoon.dfs_bfs.b13913;
 import java.util.*;
 
 public class Main_13913 {
+    static int[] visited = new int[100001];
+    static int[] move = new int[100001];
+    static int start, end;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int K = sc.nextInt();
+        start = sc.nextInt();
+        end = sc.nextInt();
 
-        int max = 100000;
+        BFS(start);
 
-        boolean[] visited = new boolean[max + 1];
+        System.out.println(visited[end]);
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder(start + " ");
 
-        Queue<Move> Q = new LinkedList<>();
-
-        visited[N] = Q.add(new Move(N, 0, new StringBuilder()));
-
-        while (!Q.isEmpty()) {
-            Move now = Q.poll();
-
-            if (now.gN() == K){
-                result(now.gC(), now.gS());
-                break;
-            }
-
-            if (now.gN() > K){
-                StringBuilder sb = now.gS();
-                int count = now.gC();
-                for (int i = now.gN(); i != K; i--) {
-                    count++;
-                    sb.append(i + " ");
-                }
-                result(count, sb);
-                break;
-            }
-
-            if (now.gN() < max && !visited[now.gN() + 1] && now.gN() < K)
-                visited[now.gN() + 1] = Q.add(new Move(
-                        now.gN() + 1, now.gC() + 1, now.gS()
-                ));
-            if (now.gN() - 1 >= 0 && !visited[now.gN() - 1])
-                visited[now.gN() - 1] = Q.add(new Move(
-                        now.gN() - 1, now.gC() + 1, now.gS()
-                ));
-            if (now.gN() * 2 < visited.length && !visited[now.gN() * 2] && now.gN() < K)
-                visited[now.gN() * 2] = Q.add(new Move(
-                        now.gN() * 2, now.gC() + 1, now.gS()
-                ));
+        int i = end;
+        while (i != start) {
+            stack.push(i);
+            i = move[i];
         }
-    }
-    static void result(int count, StringBuilder sb){
-        System.out.println(count);
+
+        while (!stack.isEmpty())
+            sb.append(stack.pop() + " ");
+
         System.out.println(sb);
     }
-    static class Move{
-        private int now, count;
-        private StringBuilder sb = new StringBuilder();
+    static void BFS(int start) {
+        Queue<Integer> Q = new LinkedList<>();
+        Q.add(start);
 
-        public int gN() {
-            return this.now;
-        }
-        public int gC() {
-            return this.count;
-        }
-        public StringBuilder gS() {
-            return this.sb;
-        }
-        public Move(int now, int count, StringBuilder sb) {
-            this.now = now;
-            this.count = count;
-            this.sb.append(sb).append(now + " ");
+        while (!Q.isEmpty()) {
+            int now = Q.poll();
+            if (now == end) break;
+
+            if (now - 1 >= 0 && visited[now - 1] == 0) {
+                visited[now - 1] = visited[now] + 1;
+                move[now - 1] = now;
+                Q.add(now - 1);
+            }
+
+            if (now + 1 < move.length && visited[now + 1] == 0) {
+                visited[now + 1] = visited[now] + 1;
+                move[now + 1] = now;
+                Q.add(now + 1);
+            }
+
+            if (now * 2 < move.length && visited[now * 2] == 0) {
+                visited[now * 2] = visited[now] + 1;
+                move[now * 2] = now;
+                Q.add(now * 2);
+            }
         }
     }
 }
