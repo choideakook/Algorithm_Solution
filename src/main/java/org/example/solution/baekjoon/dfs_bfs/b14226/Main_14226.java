faef271fb40ed921de3b8b36db53e549c024071d
choideakook <shdrnrhd113@naver.com>
2023-08-21 10:48:01 +0900

    b14226: 문제 해결 실패

--- src/main/java/org/example/solution/baekjoon/dfs_bfs/b14226/Main_14226.java
+++ src/main/java/org/example/solution/baekjoon/dfs_bfs/b14226/Main_14226.java
@@ -17,29 +17,28 @@ public class Main_14226 {
         Scanner sc = new Scanner(System.in);
         int S = sc.nextInt();
 
-        System.out.println(BFS(1, S));
+        System.out.println(BFS(2, S));
     }
     static int BFS(int start, int end) {
         boolean[] visited = new boolean[1001];
-        boolean[] copy = new boolean[1001];
         Queue<int[]> Q = new LinkedList<>();
 
-        visited[start] = Q.add(new int[] {start, 1, 1});
-        copy[start] = true;
+        visited[start] = Q.add(new int[] {start, 2, 1});
 
         while (!Q.isEmpty()) {
             int[] now = Q.poll();
 
             if (now[0] == end) return now[1];
 
+
             if (now[0] + now[2] < 1001 && !visited[now[0] + now[2]])
                 visited[now[0] + now[2]] = Q.add(
                         new int[] {now[0] + now[2], now[1] + 1, now[2]}
                 );
 
-            if (!copy[now[0]])
-                copy[now[0]] = Q.add(
-                        new int[] {now[0], now[1] + 1, now[0]}
+            if (now[0] + now[0] < 1001 && !visited[now[0] + now[0]])
+                visited[now[0] + now[0]] = Q.add(
+                        new int[] {now[0] + now[0], now[1] + 2, now[0]}
                 );
 
             if (now[0] - 1 > 2 && !visited[now[0] - 1])
