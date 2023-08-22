d8cc3a25640813676fd0884ea7e6d6d72a5ba447
choideakook <shdrnrhd113@naver.com>
2023-08-22 10:18:16 +0900

    b1744: 탐욕법으로 문제 해결

--- src/main/java/org/example/solution/baekjoon/greedy/b1744/Main_1744.java
+++ src/main/java/org/example/solution/baekjoon/greedy/b1744/Main_1744.java
@@ -10,39 +10,39 @@ package org.example.solution.baekjoon.greedy.b1744;
 import java.util.*;
 
 public class Main_1744 {
+    static int result;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int result = 0;
 
         ArrayList<Integer> plus = new ArrayList<>();
         ArrayList<Integer> minus = new ArrayList<>();
-        ArrayList<Integer> zero = new ArrayList<>();
-
+        boolean zero = false;
 
         for (int i = 0; i < N; i++) {
             int num = sc.nextInt();
+
             if (num > 1) plus.add(num);
-            else if (num == 0) zero.add(0);
+            else if (num == 1) result++;
+            else if (num == 0) zero = true;
             else minus.add(num);
         }
-
         Collections.sort(plus, Collections.reverseOrder());
         Collections.sort(minus);
 
-        for (int i = 1; i < plus.size(); i += 2)
-            result += plus.get(i - 1) * plus.get(i);
-
-        if (zero.size() > 0 && zero.size() <= minus.size() && minus.get(0) != 1)
-            for (int i : zero)
-                minus.remove(0);
+        mul(plus); mul(minus);
 
         if (plus.size() % 2 == 1)
-            minus.add(plus.get(plus.size() - 1));
+            result += plus.get(plus.size() - 1);
 
-        for (int i = 0; i < minus.size(); i++)
-            result += minus.get(i);
+        if (minus.size() % 2 == 1 && !zero)
+            result += minus.get(minus.size() - 1);
 
         System.out.println(result);
+    }
+    static void mul(ArrayList<Integer> list) {
+        for (int i = 1; i < list.size(); i += 2)
+            result += list.get(i - 1) * list.get(i);
     }
 }
