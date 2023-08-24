958048e5462d0c499aee158abacfac007cd119bb
choideakook <shdrnrhd113@naver.com>
2023-08-24 11:15:57 +0900

    fb11726: long 으로 배열 변경 / index 에 나머지로 저장 / 성공

--- src/main/java/org/example/solution/baekjoon/dp/b11726/Main_11726.java
+++ src/main/java/org/example/solution/baekjoon/dp/b11726/Main_11726.java
@@ -3,29 +3,28 @@ package org.example.solution.baekjoon.dp.b11726;
 /**
  * 2 * N 타일 채우기
  * 시간 1초
- *
+ * <p>
  * N = length ( 1 ~ 1,000 )
  */
 
 import java.util.*;
 
 public class Main_11726 {
-    static int[] D;
+    static long[] D = new long[1001];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        D = new int[N];
-        if (N > 1)
-            D[1] = 2;
+        int N = sc.nextInt() - 1;
         D[0] = 1;
+        D[1] = 2;
 
-        if (N > 2) DP(N - 1);
-        System.out.println(D[N - 1] % 10007);
+        if (N > 1) DP(N);
+        System.out.println(D[N]);
     }
     static void DP(int index) {
         if (D[index - 1] == 0)
             DP(index - 1);
 
-        D[index] = D[index - 1] + D[index - 2];
+        D[index] = (D[index - 1] + D[index - 2]) % 10007;
     }
 }
