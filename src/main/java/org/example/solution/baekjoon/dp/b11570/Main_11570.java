d082cecc10e9e438f5bf83392bc1f2babc057636
choideakook <shdrnrhd113@naver.com>
2023-08-25 14:17:21 +0900

    b11570: B 가 시작되는 index 기억하도록 수정 / 1% 에서 실패

--- src/main/java/org/example/solution/baekjoon/dp/b11570/Main_11570.java
+++ src/main/java/org/example/solution/baekjoon/dp/b11570/Main_11570.java
@@ -3,7 +3,7 @@ package org.example.solution.baekjoon.dp.b11570;
 /**
  * 환상의 듀엣
  * 시간 2초
- *
+ * <p>
  * N = length ( 1 ~ 2,000 )
  * index ( 1 ~ 1,000,000 )
  */
@@ -13,26 +13,29 @@ import java.io.*;
 
 public class Main_11570 {
     static int[] map, pre;
-    static boolean startB;
     static int A, B;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         map = new int[N];
+        int startB = 0;
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         map[0] = Integer.parseInt(st.nextToken());
-        pre = new int[] {map[0], 0};
+        pre = new int[]{map[0], 0};
 
         for (int i = 1; i < N; i++) {
             map[i] = Integer.parseInt(st.nextToken());
             int gap = Math.abs(map[i - 1] - map[i]);
-            if (pre[1] < gap) pre[1] = map[i];
+            if (pre[1] < gap) {
+                pre[1] = map[i];
+                startB = i;
+            }
         }
         for (int i = 1; i < N; i++) {
-            if (!startB)
-                startB(i);
+            if (i < startB) startB(i);
+            else if (i == startB) continue;
             else
                 compare(Math.abs(map[i] - pre[0]),
                         Math.abs(map[i] - pre[1]), map[i]);
@@ -40,20 +43,15 @@ public class Main_11570 {
         if (A + B == 0) System.out.println(1);
         else System.out.println(A + B);
     }
-    static void startB(int i){
-        if (map[i] == pre[1])
-            startB = true;
-        else {
-            A += Math.abs(pre[0] - map[i]);
-            pre[0] = map[i];
-        }
+    static void startB(int i) {
+        A += Math.abs(pre[0] - map[i]);
+        pre[0] = map[i];
     }
     static void compare(int a, int b, int index) {
         if (a >= b) {
             B += b;
             pre[1] = index;
-        }
-        else {
+        } else {
             A += a;
             pre[0] = index;
         }
