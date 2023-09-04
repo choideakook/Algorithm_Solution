00401b21c568af8474505befddacba0d8a5cac2c
choideakook <shdrnrhd113@naver.com>
2023-09-04 10:04:30 +0900

    p17680: 75.0 점 실패

---
+++ src/main/java/org/example/solution/programers/lv2/p17680/Solution_17680.java
@@ -0,0 +1,28 @@
+package org.example.solution.programers.lv2.p17680;
+
+/**
+ * [1차] 캐시
+ * cacheSize ( 1 ~ 30 )
+ * cities ( ~ 100,000 )
+ */
+
+import java.util.*;
+
+public class Solution_17680 {
+    public int solution(int cacheSize, String[] cities) {
+        Queue<String> Q = new LinkedList<>();
+        int answer = 0;
+
+        for (int i = 0; i < cities.length; i++) {
+            String city = cities[i].toLowerCase();
+
+            if (Q.contains(city)) answer++;
+            else {
+                answer += 5;
+                Q.add(city);
+                if (Q.size() > cacheSize) Q.poll();
+            }
+        }
+        return answer;
+    }
+}
---
+++ src/test/java/org/example/solution/programers/lv2/p17680/Solution_17680Test.java
@@ -0,0 +1,55 @@
+package org.example.solution.programers.lv2.p17680;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Solution_17680Test {
+    @Test
+    void no1() {
+        Solution_17680 S = new Solution_17680();
+        int size = 3;
+        String[] cities = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"};
+        int answer = 50;
+        assertThat(S.solution(size, cities)).isEqualTo(answer);
+    }
+    @Test
+    void no2() {
+        Solution_17680 S = new Solution_17680();
+        int size =3 ;
+        String[] cities = {"Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"};
+        int answer =21 ;
+        assertThat(S.solution(size, cities)).isEqualTo(answer);
+    }
+    @Test
+    void no3() {
+        Solution_17680 S = new Solution_17680();
+        int size =2 ;
+        String[] cities = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"};
+        int answer =60 ;
+        assertThat(S.solution(size, cities)).isEqualTo(answer);
+    }
+    @Test
+    void no4() {
+        Solution_17680 S = new Solution_17680();
+        int size = 5;
+        String[] cities = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"};
+        int answer = 52;
+        assertThat(S.solution(size, cities)).isEqualTo(answer);
+    }
+    @Test
+    void no5() {
+        Solution_17680 S = new Solution_17680();
+        int size = 2;
+        String[] cities = {"Jeju", "Pangyo", "NewYork", "newyork"};
+        int answer = 16;
+        assertThat(S.solution(size, cities)).isEqualTo(answer);
+    }@Test
+    void no6() {
+        Solution_17680 S = new Solution_17680();
+        int size = 0;
+        String[] cities = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA"};
+        int answer = 25;
+        assertThat(S.solution(size, cities)).isEqualTo(answer);
+    }
+}
