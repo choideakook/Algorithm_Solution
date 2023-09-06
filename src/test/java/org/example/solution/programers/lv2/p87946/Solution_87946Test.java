ff34bc50e4ef00f57ee9539afe0acb4311c5d3e9
choideakook <shdrnrhd113@naver.com>
2023-09-06 13:27:13 +0900

    p7946: 실패 테스트 케이스 발견

--- src/test/java/org/example/solution/programers/lv2/p87946/Solution_87946Test.java
+++ src/test/java/org/example/solution/programers/lv2/p87946/Solution_87946Test.java
@@ -6,11 +6,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class Solution_87946Test {
     @Test
-    void name() {
+    void no1() {
         Solution_87946 S = new Solution_87946();
         int k = 80;
         int[][] dun = {{80,20},{50,40},{30,10}};
         int a = 3;
+        assertThat(S.solution(k, dun)).isEqualTo(a);
+    }
+    @Test
+    void no2() {
+        Solution_87946 S = new Solution_87946();
+        int k = 40;
+        int[][] dun = {{40,20},{10,10},{10,10},{10,10},{10,10}};
+        int a = 4;
         assertThat(S.solution(k, dun)).isEqualTo(a);
     }
 }
