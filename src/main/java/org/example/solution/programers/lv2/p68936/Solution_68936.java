3baf0ba34e7e6d4abc0e293b9e028187e6b3455c
choideakook <shdrnrhd113@naver.com>
2023-09-18 14:02:26 +0900

    p68936: 풀이 실패

---
+++ src/main/java/org/example/solution/programers/lv2/p68936/Solution_68936.java
@@ -0,0 +1,15 @@
+package org.example.solution.programers.lv2.p68936;
+
+/**
+ * 쿼드압축 후 개수 세기
+ * arr[][] = 정사각형 ( 1 ~ 1,024 )
+ */
+
+import java.util.*;
+
+public class Solution_68936 {
+
+    public int[] solution(int[][] arr) {
+        return null;
+    }
+}
---
+++ src/test/java/org/example/solution/programers/lv2/p68936/Solution_68936Test.java
@@ -0,0 +1,36 @@
+package org.example.solution.programers.lv2.p68936;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Solution_68936Test {
+    @Test
+    void no1() {
+        Solution_68936 S = new Solution_68936();
+        int[][] arr = {
+                {1,1,0,0},
+                {1,0,0,0},
+                {1,0,0,1},
+                {1,1,1,1}
+        };
+        int[] ans = {4, 9};
+        assertThat(S.solution(arr)).isEqualTo(ans);
+    }
+    @Test
+    void no2() {
+        Solution_68936 S = new Solution_68936();
+        int[][] arr = {
+                {1,1,1,1,1,1,1,1},
+                {0,1,1,1,1,1,1,1},
+                {0,0,0,0,1,1,1,1},
+                {0,1,0,0,1,1,1,1},
+                {0,0,0,0,0,0,1,1},
+                {0,0,0,0,0,0,0,1},
+                {0,0,0,0,1,0,0,1},
+                {0,0,0,0,1,1,1,1}
+        };
+        int[] ans = {10, 15};
+        assertThat(S.solution(arr)).isEqualTo(ans);
+    }
+}
