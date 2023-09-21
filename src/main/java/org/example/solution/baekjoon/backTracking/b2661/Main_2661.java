af241344d9fb9b119b3c3af54afce316c512ea23
choideakook <shdrnrhd113@naver.com>
2023-09-21 15:24:23 +0900

    b2661: 1% 오답

--- src/main/java/org/example/solution/baekjoon/backTracking/b2661/Main_2661.java
+++ src/main/java/org/example/solution/baekjoon/backTracking/b2661/Main_2661.java
@@ -10,18 +10,21 @@ import java.util.*;
 
 public class Main_2661 {
     static int N;
-    static String answer = "12", index = "1";
+    static StringBuilder sb = new StringBuilder("12");
+    static String index = "1";
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         if (N == 1) System.out.println(1);
-        else DFS("2", index);
-        System.out.println(answer);
+        else {
+            DFS("2", index);
+            System.out.println(sb);
+        }
     }
 
     static boolean DFS(String key, String value) {
-        int aSize = answer.length();
+        int aSize = sb.length();
 
         if (N == aSize) return true;
         if (key.equals(value)) {
@@ -34,17 +37,18 @@ public class Main_2661 {
 
         if (kSize < vSize) {
             String K = index;
-            answer += index;
+            sb.append(index);
 
             if (!DFS(K, indexReset()));
                 if (!DFS(K, index))
                     if (!DFS(K, index)) {
+                        sb.deleteCharAt(sb.length() - 1);
                         return false;
                     }
         }
         else {
             int end = aSize - key.length();
-            String K = answer.substring(end - vSize, end);
+            String K = sb.substring(end - vSize, end);
             String V = key.substring(key.length() - 1) + value;
             return DFS(K, V);
         }
