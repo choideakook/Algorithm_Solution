28aa84ea89e64541d882f296a6038cea47f12e77
choideakook <shdrnrhd113@naver.com>
2023-11-22 20:53:01 +0900

    b1697: 47% 오답

---
+++ src/main/java/org/example/solution/baekjoon/dfs_bfs/b1697/Main_1697.java
@@ -0,0 +1,45 @@
+package org.example.solution.baekjoon.dfs_bfs.b1697;
+
+/**
+ * 숨바꼭질
+ * 시간 2초
+ * N = start ( 0 - 100,000 )
+ * K = end ( 0 - 100,000 )
+ * 걷기 = + 1 , - 1
+ * 순간 이동 = * 2
+ */
+
+import java.util.*;
+
+public class Main_1697 {
+    static int[] visit = new int[100001];
+    static Queue<Integer> Q = new LinkedList<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        Q.offer(K);
+
+        while (!Q.isEmpty()) {
+            Integer now = Q.poll();
+
+            if (now == N) {
+                System.out.println(visit[now]++);
+                return;
+            }
+            int min = now - 1;
+            int mul = now / 2;
+            int add = now + 1;
+
+            if (min >= 0 && visit[min] == 0) move(min, now);
+            if (now % 2 == 0 && visit[mul] == 0) move(mul, now);
+            if (add < 100001 && visit[add] == 0) move(add, now);
+        }
+    }
+
+    private static void move(int min, Integer now) {
+        visit[min] = visit[now] + 1;
+        Q.add(min);
+    }
+}
---
+++ src/test/java/org/example/solution/baekjoon/dfs_bfs/b1697/Main_1697Test.java
@@ -0,0 +1,16 @@
+package org.example.solution.baekjoon.dfs_bfs.b1697;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Main_1697Test {
+
+    @Test
+    void name() {
+        int a = 3;
+        int b = ++a;
+
+        System.out.println(b);
+    }
+}
